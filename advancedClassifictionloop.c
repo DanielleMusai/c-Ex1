562f1a1222b266dbaf7896848cdd1ad4be4e7d60
DanielleMusai <92378800+DanielleMusai@users.noreply.github.com>
2021-11-06 21:56:51 +0200

    Create advancedClassifictionloop.c

---
+++ advancedClassifictionloop.c
@@ -0,0 +1,48 @@
+#include "Numclass.h"
+#include <math.h>
+#include <stdio.h>
+
+
+ int pw (int n, int m);// pow function
+//palindrome
+int isPalindrome(int n){
+  int r,end=0,start;  
+  int flag =0;     
+start=n;    
+while(start!=0){    
+r=start%10;    
+end=(end*10)+r;    
+start=start/10;    
+}    
+if(end==n){
+    flag =1;
+}    
+
+return flag;  
+}   
+//isARMSTRONG
+int pw (int n, int m)// pow function
+{
+int num = n;
+for(int i =1 ;i < m;i++){
+num *= n;
+}
+return num;
+}
+ int isArmstrong(int n){
+   int  n2, n1, countpow = 0;
+   int flag=0;
+   float result = 0.0;
+   n2 = n;
+   for (n2 = n; n2!= 0; ++countpow) {//cheaking the num of digits
+       n2= n2/ 10;
+   }
+   for (n2 = n; n2!=0; n2 /= 10) {
+       n1 = n2 % 10;      
+      result = result +pw(n1, countpow);
+   }
+   if ((int)result == n){
+    flag=1;
+}
+return flag;
+}   
