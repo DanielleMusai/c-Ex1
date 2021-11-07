5c5eccc6383396938a884efdd201968ad24da9df
DanielleMusai <92378800+DanielleMusai@users.noreply.github.com>
2021-11-07 17:15:57 +0200

    Update Numclass.h

--- Numclass.h
+++ Numclass.h
@@ -2,17 +2,21 @@
 An Example for Armstrong num: 407  = 43 + 03 + 73  = 64 + 0 + 343  = 407 
 */ 
 int isArmstrong(int n);
+int isArmstrong1(int size,int n,int x,int s);
+int Count(int n);
+
+
 /* will return if a number is a palindrome */  
 int isPalindrome(int n);
 /* will return if a number is prime*/  
 int isPrime(int n);
  
  // An Example for a Strong num: 145  is strong number 
-1! + 4! + 5! = 145 
-*/ 
+// 1! + 4! + 5! = 145 
+
 int isStrong(int n);
 int pw (int n, int m);
-int isPl(int n);
+int isPl(int n, int end);
 int assem(int n);  
 
 
