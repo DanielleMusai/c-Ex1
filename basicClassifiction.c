fc18b2612b2366df2aec5572fc9cfce9b03b7594
DanielleMusai <92378800+DanielleMusai@users.noreply.github.com>
2021-11-07 18:01:57 +0200

    Update basicClassifiction.c

--- basicClassifiction.c
+++ basicClassifiction.c
@@ -7,7 +7,10 @@ int isPrime(int n) {
   // 0 is false
   // 1 is true 
   int i;
-  int flag=0;     
+  int flag=0;  
+ if(n==1){
+  return 1;
+ }
 for(i=2;i<=n/2;i++)    
 {    
 if(n%i==0)    {    
